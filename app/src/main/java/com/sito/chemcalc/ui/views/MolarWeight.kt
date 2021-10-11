91faa049b24aee39f031bbff6b69b82f5bec19c4
SitoNimbus <sito8943@gmail.com>
2021-10-11 16:59:24 -0700

    added MolarWeight.kt

---
+++ app/src/main/java/com/sito/chemcalc/ui/views/MolarWeight.kt
@@ -0,0 +1,47 @@
+import androidx.compose.foundation.layout.*
+import androidx.compose.material.Text
+import androidx.compose.material.TextField
+import androidx.compose.material.TextFieldDefaults
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.input.TextFieldValue
+import androidx.compose.ui.tooling.preview.Preview
+import com.sito.chemcalc.R
+
+import androidx.compose.ui.unit.dp
+
+@Composable
+fun MolarWeight() {
+
+    val formula = remember {
+        mutableStateOf(TextFieldValue())
+    }
+
+    Column(
+        modifier = Modifier
+            .fillMaxSize()
+            .padding(5.dp)
+    ) {
+        TextField(
+            placeholder = { Text(text = stringResource(id = R.string.mainInputPlaceholder)) },
+            colors = TextFieldDefaults.outlinedTextFieldColors(
+                focusedBorderColor = Color.Blue,
+                unfocusedBorderColor = Color.Gray
+            ),
+            modifier = Modifier
+                .fillMaxWidth(),
+            value = formula.value,
+            onValueChange = { formula.value = it })
+
+    }
+}
+
+@Preview
+@Composable
+fun ShowMolarWeight() {
+    MolarWeight()
+}
